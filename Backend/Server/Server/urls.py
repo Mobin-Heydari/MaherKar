03da79a648bd7717b84f1752c492a6e1328cc5f3
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-03-20 12:48:35 +0330

    The urls.py file modified.

--- Backend/Server/Server/urls.py
+++ Backend/Server/Server/urls.py
@@ -8,5 +8,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('User.urls', namespace='User')),
-
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
