562360aed8d2b066d53c73095dc133369905e026
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-03-22 14:01:23 +0330

    THe models updated and the adding Province and Cities opration automated using json file and commands.

--- Backend/Server/Locations/models.py
+++ Backend/Server/Locations/models.py
@@ -1,34 +1,19 @@
 from django.db import models
 from django.utils.text import slugify  # برای ایجاد اسلاگ از نام‌ها
 
+
+
+
 class Province(models.Model):
     name = models.CharField(max_length=100, unique=True)  # نام استان
-    slug = models.SlugField(max_length=100, unique=True, blank=True)  # اسلاگ مربوط به استان
 
     def __str__(self):
         return self.name
-
-    def save(self, *args, **kwargs):
-        """
-        قبل از ذخیره‌سازی، فیلد اسلاگ را بر اساس نام استان تنظیم می‌کند.
-        """
-        if not self.slug:
-            self.slug = slugify(self.name)
-        super(Province, self).save(*args, **kwargs)
 
 
 class City(models.Model):
     province = models.ForeignKey(Province, on_delete=models.CASCADE)  # ارتباط شهر با استان
-    name = models.CharField(max_length=100, unique=True)  # نام شهر
-    slug = models.SlugField(max_length=100, unique=True, blank=True)  # اسلاگ مربوط به شهر
+    name = models.CharField(max_length=100)  # نام شهر
 
     def __str__(self):
         return self.name
-
-    def save(self, *args, **kwargs):
-        """
-        قبل از ذخیره‌سازی، فیلد اسلاگ را بر اساس نام شهر تنظیم می‌کند.
-        """
-        if not self.slug:
-            self.slug = slugify(self.name)
-        super(City, self).save(*args, **kwargs)
