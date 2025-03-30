baa4aa818523aa4857199e7b8198bc21e22501c2
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-03-30 16:09:43 +0330

    JobseekerResumeAdvertisement model added.

--- Backend/Server/Advertisements/models.py
+++ Backend/Server/Advertisements/models.py
@@ -165,6 +165,148 @@ class JobAdvertisement(models.Model):
 
 
 
+class JobseekerResumeAdvertisement(models.Model):
+
+    class SalaryChoices(models.TextChoices):
+        RANGE_5_TO_10 = '5 to 10', '5 تا 10 میلیون تومان'
+        RANGE_10_TO_15 = '10 to 15', '10 تا 15 میلیون تومان'
+        RANGE_15_TO_20 = '15 to 20', '15 تا 20 میلیون تومان'
+        RANGE_20_TO_30 = '20 to 30', '20 تا 30 میلیون تومان'
+        RANGE_30_TO_50 = '30 to 50', '30 تا 50 میلیون تومان'
+        MORE_THAN_50 = 'More than 50', 'بیش از 50 میلیون تومان'
+        NEGOTIABLE = 'Negotiable', 'توافقی'
+
+    class GenderChoices(models.TextChoices):
+        MALE = 'Male', 'مرد'
+        FEMALE = 'Female', 'زن'
+
+    class SoldierStatusChoices(models.TextChoices):
+        COMPLETED = 'Completed', 'پایان خدمت'
+        PERMANENT_EXEMPTION = 'Permanent Exemption', 'معافیت دائم'
+        EDUCATIONAL_EXEMPTION = 'Educational Exemption', 'معافیت تحصیلی'
+        NOT_COMPLETED = 'Not Completed', 'نااتمام'
+
+    class DegreeChoices(models.TextChoices):
+        BELOW_DIPLOMA = 'Below Diploma', 'زیر دیپلم'
+        DIPLOMA = 'Diploma', 'دیپلم'
+        ASSOCIATE = 'Associate', 'فوق دیپلم'
+        BACHELOR = 'Bachelor', 'لیسانس'
+        MASTER = 'Master', 'فوق لیسانس'
+        DOCTORATE = 'Doctorate', 'دکترا'
+
+    class CooperationTypeChoices(models.TextChoices):
+        NO_EXPERIENCE = 'No EXPERIENCE', 'بدون سابقه کار'
+        LESS_THAN_3_YEARS = 'Less than Three', 'کمتر از 3 سال'
+        BETWEEN_3_AND_6_YEARS = 'Three or More', '3 تا 6 سال'
+        MORE_THAN_6_YEARS = 'Six or More', 'بیشتر از 6 سال'
+
+    class JobTypeChoices(models.TextChoices):
+        FULL_TIME = 'Full-Time', 'تمام وقت'
+        PART_TIME = 'Part-Time', 'پاره وقت'
+        REMOTE = 'Remote', 'دورکاری'
+        INTERNSHIP = 'Internship', 'کارآموزی'
+    
+    class StatusChoices(models.TextChoices):
+        PENDING = 'Pending', 'در حال بررسی'
+        APPROVED = 'Approved', 'تایید شده'
+        REJECTED = 'Rejected', 'رد شده'
+    
+    job_seeker_profile = models.ForeignKey(
+        JobSeekerProfile,
+        on_delete=models.CASCADE,
+        verbose_name="پروفایل کار جو",
+        related_name="JobSeeker_Profile_Advertisements"
+    )
+
+    resume = models.ForeignKey(
+        JobSeekerResume,
+        on_delete=models.CASCADE,
+        verbose_name="رزومه",
+        related_name="Resume_Advertisements"
+    )
+
+    title = models.CharField(
+        max_length=255,
+        verbose_name="عنوان آگهی",
+        blank=True,
+        null=True,
+        help_text="یک عنوان کوتاه برای آگهی"
+    )
+
+    industry = models.ForeignKey(
+        Industry,
+        on_delete=models.CASCADE,
+        related_name="Resume_Advertisements",
+    )
+
+    location = models.ForeignKey(
+        City,
+        on_delete=models.CASCADE,
+        related_name="Resume_Advertisements",
+    )
+
+    slug = models.SlugField(
+        max_length=255,
+        unique=True,
+        verbose_name="اسلاگ",
+    )
+
+    description = models.TextField(
+        verbose_name="شرح آگهی",
+        blank=True,
+        null=True,
+        help_text="توضیحات بیشتر درباره آگهی"
+    )
+
+    status = models.CharField(
+        max_length=20,
+        verbose_name="وضعیت آکهی",
+        default=StatusChoices.PENDING
+    )
+
+    gender = models.CharField(
+        max_length=20,
+        choices=GenderChoices.choices,
+        verbose_name="جنسیت",
+    )
+
+    soldier_status = models.CharField(
+        max_length=30,
+        choices=SoldierStatusChoices.choices,
+        verbose_name="وضعیت سربازی",
+    )
+
+    degree = models.CharField(
+        max_length=20,
+        choices=DegreeChoices.choices,
+        verbose_name="حداقل مدرک تحصیلی",
+    )
+
+    experience = models.CharField(
+        max_length=50,
+        choices=CooperationTypeChoices.choices,
+        verbose_name="حداقل سابقه کاری",
+    )
+
+    salary = models.CharField(
+        max_length=20,
+        choices=SalaryChoices.choices,
+        verbose_name="محدوده حقوق",
+        default=SalaryChoices.NEGOTIABLE
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="تاریخ ایجاد",
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name="تاریخ بروزرسانی",
+    )
+
+    def __str__(self):
+        return self.title if self.title else f"آگهی رزومه #{self.pk}"
 
 
 class Application(models.Model):
