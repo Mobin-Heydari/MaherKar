ddd05aa70c8401fcb91750a17f7afd6a8a45caaf
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-03-30 16:32:23 +0330

    JobseekerResumeAdvertisementRouter added and the urls added.

--- Backend/Server/Advertisements/routers.py
+++ Backend/Server/Advertisements/routers.py
@@ -1,6 +1,6 @@
 from rest_framework import routers
 from django.urls import path, include
-from Advertisements.views import JobAdvertisementViewSet, ApplicationViewSet
+from Advertisements.views import JobAdvertisementViewSet, ApplicationViewSet, JobseekerResumeAdvertisementViewSet
 
 
 
@@ -34,6 +34,24 @@ class ApplicationRouter(routers.DefaultRouter):
                 path('', ApplicationViewSet.as_view({'get': 'list', 'post': 'create'})),
                 path('<int:pk>/', include([
                     path('', ApplicationViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
+                ])),
+            ])),
+        ]
+        return urls + custom_urls
+
+
+class JobseekerResumeAdvertisementRouter(routers.DefaultRouter):
+    def __init__(self):
+        super().__init__()
+        self.register(r'', JobseekerResumeAdvertisementViewSet, basename='applications')
+
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+            path('', include([
+                path('', JobseekerResumeAdvertisementViewSet.as_view({'get': 'list', 'post': 'create'})),
+                path('<slug:slug>/', include([
+                    path('', JobseekerResumeAdvertisementViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
                 ])),
             ])),
         ]
