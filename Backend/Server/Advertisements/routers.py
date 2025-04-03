150712cb9b675e878f07f494c4d611c1ebcb1420
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-04-03 18:47:56 +0330

    The Job and Resume ads routers and urls added.

--- Backend/Server/Advertisements/routers.py
+++ Backend/Server/Advertisements/routers.py
@@ -1,6 +1,6 @@
 from rest_framework import routers
 from django.urls import path, include
-from Advertisements.views import AdvertisementViewSet, ApplicationViewSet
+from Advertisements.views import AdvertisementViewSet, JobAdvertisementViewSet, ResumeAdvertisementViewSet, ApplicationViewSet
 
 
 
@@ -18,6 +18,42 @@ class AdvertisementRouter(routers.DefaultRouter):
                 path('<slug:slug>/', include([
                     path('', AdvertisementViewSet.as_view({'get': 'retrieve', 'put': 'update'})),
                 ])),
+            ])),
+        ]
+        return urls + custom_urls
+
+
+class JobAdvertisementRouter(routers.DefaultRouter):
+    def __init__(self):
+        super().__init__()
+        self.register(r'', JobAdvertisementViewSet, basename='JobAdvertisements')
+
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+            path('', include([
+                path('', JobAdvertisementViewSet.as_view({'get': 'list'})),
+                path('<int:pk>/', JobAdvertisementViewSet.as_view({'get': 'update'})),
+                path('<slug:slug>/', JobAdvertisementViewSet.as_view({'get': 'retrieve', 'post': 'create'})),
+                path('<int:pk>/<slug:slug>/', JobAdvertisementViewSet.as_view({'get': 'update'})),
+            ])),
+        ]
+        return urls + custom_urls
+
+
+class ResumeAdvertisementRouter(routers.DefaultRouter):
+    def __init__(self):
+        super().__init__()
+        self.register(r'', ResumeAdvertisementViewSet, basename='ResumeAdvertisements')
+
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+            path('', include([
+                path('', ResumeAdvertisementViewSet.as_view({'get': 'list', 'post': 'create'})),
+                path('<int:pk>/', ResumeAdvertisementViewSet.as_view({'get': 'update'})),
+                path('<slug:slug>/', ResumeAdvertisementViewSet.as_view({'get': 'retrieve'})),
+                path('<int:pk>/<slug:slug>/', ResumeAdvertisementViewSet.as_view({'get': 'update'})),
             ])),
         ]
         return urls + custom_urls
--- Backend/Server/Advertisements/urls.py
+++ Backend/Server/Advertisements/urls.py
@@ -1,14 +1,18 @@
 from django.urls import path, include
-from Advertisements.routers import AdvertisementRouter, ApplicationRouter
+from Advertisements.routers import AdvertisementRouter, JobAdvertisementRouter, ResumeAdvertisementRouter, ApplicationRouter
 
 
 app_name = "Advertisements"
 
 ad_router = AdvertisementRouter()
+job_ad_router = AdvertisementRouter()
+resume_ad_router = AdvertisementRouter()
 applications_router = ApplicationRouter()
 
 
 urlpatterns = [
     path('advertisments/', include(ad_router.get_urls())),
+    path('job/', include(job_ad_router.get_urls())),
+    path('resume/', include(resume_ad_router.get_urls())),
     path('applications/', include(applications_router.get_urls()))
 ]
