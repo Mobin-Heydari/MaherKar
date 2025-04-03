c0b164fa49155c773aa9689ccf578b9c26b67425
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-04-03 10:07:25 +0330

    THe Advertisements router and urls added.

--- Backend/Server/Advertisements/routers.py
+++ Backend/Server/Advertisements/routers.py
@@ -1,7 +1,26 @@
 from rest_framework import routers
 from django.urls import path, include
-from Advertisements.views import ApplicationViewSet
+from Advertisements.views import AdvertisementViewSet, ApplicationViewSet
 
+
+
+
+class AdvertisementRouter(routers.DefaultRouter):
+    def __init__(self):
+        super().__init__()
+        self.register(r'', AdvertisementViewSet, basename='Advertisements')
+
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+            path('', include([
+                path('', AdvertisementViewSet.as_view({'get': 'list'})),
+                path('<slug:slug>/', include([
+                    path('', AdvertisementViewSet.as_view({'get': 'retrieve', 'put': 'update'})),
+                ])),
+            ])),
+        ]
+        return urls + custom_urls
 
 
 class ApplicationRouter(routers.DefaultRouter):
--- Backend/Server/Advertisements/urls.py
+++ Backend/Server/Advertisements/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path, include
-from Advertisements.routers import ApplicationRouter
+from Advertisements.routers import AdvertisementRouter, ApplicationRouter
 
 
 app_name = "Advertisements"
 
-
+ad_router = AdvertisementRouter()
 applications_router = ApplicationRouter()
 
 
 urlpatterns = [
-    path('applications/', include(applications_router.get_urls())),
+    path('advertisments/', include(ad_router.get_urls())),
+    path('applications/', include(applications_router.get_urls()))
 ]
