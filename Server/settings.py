987da5bd2d2cd691a36cced3c857b4b2f7296ab5
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-04-13 14:47:36 +0330

    The project is ready for deploying to the liara.

--- Server/settings.py
+++ Server/settings.py
@@ -126,8 +126,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
+
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'statics')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
