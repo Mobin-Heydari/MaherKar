ddc192f688a9e04f34424b415ff948206dfe73a8
Mobin_Heydari <mobinheydari.developer@gmail.com>
2025-04-29 11:27:28 +0330

    The bug fixed.

--- Backend/Server/Server/settings.py
+++ Backend/Server/Server/settings.py
@@ -56,6 +56,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'Server.urls'
